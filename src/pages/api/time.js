5f2fa6dbed2bc2c4788e53016257c3b358f36539
DiegoFmDev <darkzeus842@gmail.com>
2024-12-10 21:18:57 -0400

    Eliminar todos los archivos del repositorio remoto

--- src/pages/api/time.js
+++
@@ -1,9 +0,0 @@
-const handler = async (_, res) => {
-  const response = await fetch(
-    `https://api.sunrise-sunset.org/json?lat=-17.798073&lng=-63.192509&formatted=0`
-  ).then((res) => res.json());
-    
-  res.status(200).json(response);
-};
-
-export default handler;
